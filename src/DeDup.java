a16de4c63909e676d66a27d5450f0ed5c524cf26
lkavitha <kavitha.lingareddy@gmail.com>
2017-05-09 08:30:53 -0400

    Test Changes

--- src/DeDup.java
+++ src/DeDup.java
@@ -40,8 +40,7 @@ public class DeDup {
 
 
 		int[] testArray={1,2,34,34,25,1,45,3,26,85,4,34,86,25,43,2,1,10000,11,16,19,1,18,4,9,3, 13,5,11,4,7,19,16,5,9,12,3,20,7,15,17,10,6,1,8,18,4,14,13,2,11};
-		int[] testArray1=null;
-		DeDup de=new DeDup(testArray1);
+		DeDup de=new DeDup(testArray);
 
 		int[] res=null;
 		
